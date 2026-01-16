b471f92bb28a6056f598d03804d3b39e1ac752de
dheim123 <dheim@midland.k12.ia.us>
2026-01-16 11:50:45 -0600

    Implement PriceCheck and GuessNumber methods
    Added PriceCheck and GuessNumber methods to Store class.

--- Store.cs
+++ Store.cs
@@ -1,5 +1,26 @@
 ﻿namespace knightmoves;
 public class Store
 {
-    // Add your code here
+    public string PriceCheck("price"){
+        if (price == 1.50){
+            return "match";
+        }
+
+        if (price == 1.00){
+            return "";
+        }
+        
+    }
+
+    public string GuessNumber("guess"){
+        if (guess != 25){
+            return "not it";
+        }
+
+        if (guess == 25){
+            return "";
+        }
+    }
+    
 }
+
